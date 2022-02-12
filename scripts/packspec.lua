5de5c164f6eaa357ed9ac56dd75c70bcec2ca54b
ii14 <ii14@users.noreply.github.com>
2022-02-12 12:32:29 +0100

    Evaluate example in a sandbox, use global variables
    - Evaluate plugspec.1.lua in a sandbox
    - Use global variables, instead of returning a table
    - Refactor validator

--- scripts/packspec.lua
+++ scripts/packspec.lua
@@ -1,24 +1,38 @@
 #! /usr/bin/env lua5.1
 
+local function fatal(s, ...)
+  print(string.format(s, ...))
+  os.exit(1)
+end
+
+local path = arg[1]
+if not path then
+  fatal('usage: %s <file>', arg[0])
+end
+
 local jsonschema = require('jsonschema')
 local schema = require('packspec.schema')
 -- local inspect = require('inspect')
 
 local validator = jsonschema.generate_validator(schema)
 
-local path = arg[1]
-
-local spec_chunk, err = loadfile(path)
+local chunk, err = loadfile(path)
 if err then
-  error(err)
+  fatal('%s: loading failed\n%s', path, err)
 end
 
-local spec = spec_chunk()
+local spec = {}
+setfenv(chunk, spec)
 
 local ok
-ok, err = validator(spec)
-
+ok, err = pcall(chunk)
 if not ok then
-  error(err)
+  fatal('%s: evaluation failed\n%s', path, err)
 end
-print(string.format('%s successfully validated', path))
+
+ok, err = validator(spec)
+if not ok then
+  fatal('%s: validation failed\n%s', path, err)
+end
+
+print(string.format('%s: OK', path))
