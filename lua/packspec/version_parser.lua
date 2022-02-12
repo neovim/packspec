e3c699556bc28a68e4bc8195691287011ac55276
ii14 <ii14@users.noreply.github.com>
2022-02-12 08:42:41 +0100

    Add more comments

--- lua/packspec/version_parser.lua
+++ lua/packspec/version_parser.lua
@@ -1,21 +1,28 @@
+-- Example version parser implementation
+
 local parse = {}
 
----@alias Version       number[]
----@alias Operator      "==" | "~=" | "<" | ">" | "<=" | ">=" | "~>"
----@alias VersionExpr   { [1]: Version, [2]: Operator|nil }
+---@alias Version   number[]
+---@alias Operator  "==" | "~=" | "<" | ">" | "<=" | ">=" | "~>"
+---@alias Range     { [1]: Version, [2]: Operator|nil }
 
+--- Parse version
 ---@param s string
 ---@return Version
 function parse.version(s)
   assert(type(s) == "string", "expected string")
+
+  -- trim leading and trailing whitespace
   s = assert(s:match("^%s*(.-)%s*$"), "invalid version")
 
   local v = {}
   while true do
+    -- parse number
     local p = assert(s:match("^%d+"), "invalid version")
     table.insert(v, tonumber(p))
     s = s:sub(#p + 1)
 
+    -- try to parse next number
     if #s == 0 then break end
     assert(s:match("^%."), "invalid version")
     s = s:sub(2)
@@ -23,8 +30,9 @@ function parse.version(s)
   return v
 end
 
+--- Parse version ranges
 ---@param s string
----@return VersionExpr[]
+---@return Range[]
 function parse.version_range(s)
   assert(type(s) == "string", "expected string")
   local res = {}
@@ -43,7 +51,7 @@ function parse.version_range(s)
     local v = assert(s:match("^[%d%.]+"), "expected version")
     table.insert(res, { parse.version(v), op })
 
-    -- next expression
+    -- try to parse next range
     s = s:sub(#v + 1):match("^%s*(%S.*)")
     if not s then break end
     assert(s:match("^,"), "trailing characters")
