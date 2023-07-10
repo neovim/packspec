fa7ef1c67266784d0c8183e02e0c39e9d7e3e35c
Justin M. Keyes <justinkz@gmail.com>
2023-07-10 14:49:57 +0200

    fix: generate_json_schema.lua removes final newline
    Problem:
    "generate_json_schema.lua" removes final newline from
    "schema/packspec_schema.json".
    
    Solution:
    Append a newline before writing.

--- scripts/generate_json_schema.lua
+++ scripts/generate_json_schema.lua
@@ -3,7 +3,7 @@
 -- local cjson = require('cjson')
 local schema = require('packspec.schema')
 local format_json = require('utils.format_cjson')
-local encoded_json = format_json(schema, "\n", "  ")
+local encoded_json = format_json(schema, "\n", "  ")..'\n'
 
 local file = io.open('schema/packspec_schema.json', 'w')
 
