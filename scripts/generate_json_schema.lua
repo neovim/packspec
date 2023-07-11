faa640928a9274a2a567d26fa16b8a635a2831dc
Justin M. Keyes <justinkz@gmail.com>
2023-07-11 11:26:29 +0200

    use "nvim -l" instead of "lua"

--- scripts/generate_json_schema.lua
+++ scripts/generate_json_schema.lua
@@ -1,12 +1,9 @@
-#! /usr/bin/env lua5.1
-
 -- local cjson = require('cjson')
 local schema = require('packspec.schema')
 local format_json = require('utils.format_cjson')
+
 local encoded_json = format_json(schema, "\n", "  ")..'\n'
-
-local file = io.open('schema/packspec_schema.json', 'w')
-
+local file = assert(io.open('schema/packspec_schema.json', 'w'))
 io.output(file)
 io.write(encoded_json)
 io.close(file)
